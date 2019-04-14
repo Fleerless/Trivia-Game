a0f4dd4d7f57deaa2697abaaf6292a026ca3c190
Jeffrey Fleer <Jeff@as-MacBook-Pro.local>
2019-04-13 21:42:52 -0600

    all basic game logic works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,56 @@
 $(document).ready(function(){
 
-var questions = ["Question 1", "Question 2", "Question 3"];
+var questions = [["Question 1", "1"], ["Question 2", "2"], ["Question 3", "3"]];
 var choices = [["Answeer 1", "Answer 2", "answer 3", "Answer 4"], ["Answeer 1", "answer 2", "Answer 3", "Answer 4"],["Answeer 1", "Answer 2", "answer 3", "Answer 4"], ["Answeer 1", "Answer 2", "answer 3", "Answer 4"]];
 var qIndex = 0;
 var time=3;
 var countdown;
+var correct = 0;
+var wrong = 0;
+var clickedAnswer;
 
 function startGame() {
-    $("#question").html(questions[qIndex]);
+    $("#question").html(questions[qIndex][0]);
     $("#choices").empty();
     countdown = setInterval(timer, 1000);
     $.each(choices[qIndex], function(index, value){
         var div = $("<div>");
-        div.attr("number", index);
+        div.attr("number", (index+1));
+        div.attr("class", "buttons");
         div.text(value);
         $("#choices").append(div);
         $("#timer").text(time+ " seconds left to answer.");
-        
+        });
+        $(".buttons").click(function(){
+            clickedAnswer = $(this).attr("number");
     }) 
 };
-startGame()
+startGame();
 
 function timer(){
-    $("#timer").text(time+ " seconds left to answer.")
     time--;
+    $("#timer").text(time+ " seconds left to answer.")
+    endGame();  
     if (time === 0) {
+        getScore();
         time=3;
         qIndex++;
-        $("#question").html(questions[qIndex]);
+        clickedAnswer = "";
+        if (qIndex < questions.length){
+        $("#question").html(questions[qIndex][0]);
         $("#choices").empty();
         $.each(choices[qIndex], function(index, value){
         var div = $("<div>");
-        div.attr("number", index);
+        div.attr("number", (index+1));
+        div.attr("class", "buttons");
         div.text(value);
         $("#choices").append(div);
         $("#timer").text(time+ " seconds left to answer.");
-        }) 
-        console.log(qIndex, questions.length);     
-        endGame();    
-    }
+        }); 
+        $(".buttons").click(function(){
+            clickedAnswer = $(this).attr("number");
+    });
+    }}
 };
 
 function endGame() {
@@ -47,7 +59,31 @@ function endGame() {
         $("#choices").empty();
         $("#question").empty();
         $("#timer").empty();
-        console.log("game ended")
-    }
+        var winDiv = $("<div>");
+        winDiv.text("You got "+correct+ " questions correct!");
+        var loseDiv = $("<div>");
+        loseDiv.text("You missed " +wrong+ " questions.");
+        var restart = $("<button>");
+        restart.attr("id", "button")
+        restart.text("RESTART")
+        $("#score").append(winDiv);
+        $("#score").append(loseDiv);
+        $("#question").append(restart);
+        $("#button").click(function(){
+            $("#question").empty();
+            $("#score").empty();
+            qIndex=0;
+            correct=0;
+            wrong=0;
+            startGame();
+        })
 }
+};
+function getScore(){
+    if (clickedAnswer === questions[qIndex][1]){
+        correct++
+    } else {
+        wrong++;
+    }
+    };
 })
